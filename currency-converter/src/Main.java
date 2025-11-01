a2158f238cb111edabaf2eaea7d00072de3846d9
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-11-01 18:24:42 -0300

    T001: feat (exchange rates) - Implemented API call to fetch exchange rates and display conversion rates

--- currency-converter/src/Main.java
+++ currency-converter/src/Main.java
@@ -1,15 +1,52 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+package com.alanryan.currencyconverter;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Main {
+
+    // Classe para representar o JSON retornado pela API
+    public static class ExchangeRateResponse {
+        String result;
+        String documentation;
+        @SerializedName("base_code")
+        String baseCode;
+        @SerializedName("conversion_rates")
+        java.util.Map<String, Double> conversionRates;
+    }
+
+    public static void main(String[] args) {
+
+        String apiKey = "27e0e0261fabe51366495de9"; // sua API key
+        String baseCurrency = "USD";
+
+        String url = "https://v6.exchangerate-api.com/v6/" + apiKey + "/latest/" + baseCurrency;
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            String jsonResponse = response.body();
+
+            Gson gson = new Gson();
+            ExchangeRateResponse exchangeRateResponse = gson.fromJson(jsonResponse, ExchangeRateResponse.class);
+
+            System.out.println("Base Currency: " + exchangeRateResponse.baseCode);
+            System.out.println("Conversion Rates:");
+            exchangeRateResponse.conversionRates.forEach((currency, rate) ->
+                    System.out.println(currency + " -> " + rate));
+
+        } catch (IOException | InterruptedException e) {
+            System.out.println("Erro ao obter dados da API: " + e.getMessage());
         }
     }
 }
